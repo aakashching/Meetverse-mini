9fc289293d94e80bfa6f610d93dcc8e89c093da8
aakashching <aakashching@gmail.com>
2022-07-24 10:18:17 +0530

    cross-origin

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
+// import {Server} from 'socket.io'
 const express = require('express')
 const app = express()
 const httpServer = require('http').createServer(app)
-const io = require('socket.io')(httpServer)
+const io = require('socket.io')(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
 require('./routes')(app)
 
 require('./socketController')(io)
-
+// app.use(cros)
 const port = process.env.PORT || 3000;
 
+// app.all("*",(req,res,next)=>{
+//     res.setHeader()
+// })
 
-httpServer.listen(port,()=>{
-console.log('server is running on port 3000');
+httpServer.listen(port, () => {
+    console.log('server is running on port 3000');
 })
